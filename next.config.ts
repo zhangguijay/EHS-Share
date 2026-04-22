290f633b086689e4bf7acb178d13ae247b81063f
zhangguijay <42427750+zhangguijay@users.noreply.github.com>
2026-04-22 20:48:14 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
-  /* config options here */
+  // 开启独立打包模式，这是 Docker 部署必须的
+  output: 'standalone', 
+  
   allowedDevOrigins: ['*.dev.coze.site'],
   images: {
     remotePatterns: [
